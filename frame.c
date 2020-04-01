7e5afbd33581ce0f895e3af222f029dd41dfec7c
Brad Davidson <brad@oatmail.org>
2020-04-01 11:23:20 -0700

    Fix source address check for broadcast packets

--- frame.c
+++ frame.c
@@ -2586,8 +2586,10 @@ void do_receive_frame(faifa_t *faifa, void *buf, int len, void *UNUSED(user))
 	if (!(*eth_type == ntohs(ETHERTYPE_HOMEPLUG)) && !(*eth_type == ntohs(ETHERTYPE_HOMEPLUG_AV)))
 		return;
 
-	/* Check source address */
-	if (memcmp(eth_header->ether_shost, faifa->dst_addr, ETHER_ADDR_LEN)) {
+	/* Check source address, unless we sent to either of the broadcast addresses */
+	if (memcmp(broadcast_macaddr, faifa->dst_addr, ETHER_ADDR_LEN) &&
+            memcmp(hpav_intellon_macaddr, faifa->dst_addr, ETHER_ADDR_LEN) &&
+	    memcmp(eth_header->ether_shost, faifa->dst_addr, ETHER_ADDR_LEN)) {
 		return;
 	}
 
